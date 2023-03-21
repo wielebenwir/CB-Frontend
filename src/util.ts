eac5c7e530c5aa9a79d6edf0b3eccbce369a8ba3
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-03-21 17:11:12 +0100

    feat: add useGroupBy use-function

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,5 @@
+import { computed, ComputedRef, Ref } from 'vue';
+
 export function delay(timeInSeconds: number) {
   return new Promise((resolve) => {
     setTimeout(() => {
@@ -12,4 +14,20 @@ export async function asyncIterableToArray<T>(iterable: AsyncIterable<T>): Promi
     result.push(item);
   }
   return result;
+}
+
+export function useGroupBy<KeyType, ItemType>(
+  iterable: Ref<ItemType[]>,
+  keyFn: (item: ItemType) => KeyType,
+): ComputedRef<Map<KeyType, ItemType[]>> {
+  return computed(() => {
+    const result = new Map<KeyType, ItemType[]>();
+    for (const item of iterable.value) {
+      const key = keyFn(item);
+      const items = result.has(key) ? (result.get(key) as ItemType[]) : [];
+      items.push(item);
+      result.set(key, items);
+    }
+    return result;
+  });
 }
