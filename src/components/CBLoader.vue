cf73a9983fde4143eb63be22ce5396ff19427ce4
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-03-23 19:15:33 +0100

    feat: add loader component

---
+++ src/components/CBLoader.vue
@@ -0,0 +1,20 @@
+<template>
+  <span
+    class="cb-loader pointer-events-none w-9 h-9 tw-c-flex-center"
+    :aria-live="label ? 'polite' : undefined"
+  >
+    <img
+      class="animate-spin w-6 h-6"
+      style="animation-duration: 1.5s"
+      alt=""
+      src="../assets/loader.svg"
+    />
+    <span v-if="label" class="sr-only">{{ label }}</span>
+  </span>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  label?: string;
+}>();
+</script>
