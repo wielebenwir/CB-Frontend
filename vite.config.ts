bf3ff41a8b16040c2adbd66868a21a435c721a43
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-03-31 00:15:10 +0200

    feat(DX): temporarily enable devtools in production

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   base: '',
   plugins: [vue()],
+  define: {
+    // TODO: We might want to remove this once this library is in production use.
+    __VUE_PROD_DEVTOOLS__: true,
+  },
   build: {
     outDir: 'dist-app',
     sourcemap: true,
