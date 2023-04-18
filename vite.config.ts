1589d22770ffe1a646d4136440ddf051e7f53ae8
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-04-18 13:03:05 +0200

    fix: don’t clear screen when starting vite dev server

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '',
+  clearScreen: false,
   plugins: [vue()],
   define: {
     // TODO: We might want to remove this once this library is in production use.
