9b90688de2de642ed456e37106dbd4c5f008569e
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-04-19 21:59:19 +0200

    fix: add missing ts vue file declaration
    Without this declaration imports of vue files in typescript files will
    cause some IDEs to show errors for missing types.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-svg-loader" />
+
+declare module '*.vue' {
+  import { defineComponent } from 'vue';
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
+}
