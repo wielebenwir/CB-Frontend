822a69a6d8fd394e237956e78def626d19043b4b
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-04-20 14:37:24 +0200

    fixup! refactor: use SVG icon components instead of img elements

--- src/components/CBLoader.vue
+++ src/components/CBLoader.vue
@@ -3,17 +3,18 @@
     class="cb-loader tw-pointer-events-none tw-w-9 tw-h-9 tw-cb-flex-center"
     :aria-live="label ? 'polite' : undefined"
   >
-    <img
+    <IconLoader
       class="tw-animate-spin tw-w-6 tw-h-6"
       style="animation-duration: 1.5s"
-      alt=""
-      src="../assets/loader.svg"
+      role="presentation"
     />
     <span v-if="label" class="tw-sr-only">{{ label }}</span>
   </span>
 </template>
 
 <script lang="ts" setup>
+import { IconLoader } from '../icons';
+
 defineProps<{
   label?: string;
 }>();
