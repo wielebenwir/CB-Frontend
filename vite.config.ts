206e6e1665373ae687a08c5964cb6b5261045be4
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-04-23 16:58:43 +0200

    fix: make sure `npm run dev` works w/o config
    Since introducing the library build mode one had to set
    VITE_BUILD_MODE=app before starting the dev server.
    
    This shouldn’t be necessary.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import appConfig from './vite.cb-app-config';
 import libConfig from './vite.cb-lib-config';
 
-const VITE_BUILD_MODE = process.env.VITE_BUILD_MODE ?? 'lib';
+const VITE_BUILD_MODE = process.env.VITE_BUILD_MODE ?? 'app';
 
 if (!['app', 'lib'].includes(VITE_BUILD_MODE)) {
   throw new TypeError(`BUILD_MODE must be either 'app' or 'lib'.`);
