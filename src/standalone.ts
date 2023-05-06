c6ccf73bc1af61ea486c5b0475fc60beb80c3600
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-05-06 17:35:12 +0200

    fix: add i18n support to root app
    Without it we can’t use translations in the app-part of the cb-frontend
    lib.

--- src/standalone.ts
+++ src/standalone.ts
@@ -3,7 +3,10 @@
  * Changes to this file have no effect when this project is used as a component library.
  */
 
+import { createI18n } from '@rokoli/vue-tiny-i18n';
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+createApp(App)
+  .use(createI18n({ locale: navigator.language }))
+  .mount('#app');
