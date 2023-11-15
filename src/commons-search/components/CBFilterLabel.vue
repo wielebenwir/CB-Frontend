e4dbc32b7fc7ca61e2faf08cdb383fa2592a8fe8
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2023-11-15 21:19:08 +0100

    feat: allow custom HTML for filter labels

--- src/commons-search/components/CBFilterLabel.vue
+++ src/commons-search/components/CBFilterLabel.vue
@@ -1,5 +1,6 @@
 <template>
-  <p class="tw-font-bold tw-m-0">{{ label }}</p>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <p class="tw-font-bold tw-m-0" v-html="label" />
 </template>
 
 <script lang="ts" setup>
